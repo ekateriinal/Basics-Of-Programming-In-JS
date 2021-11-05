ac3a6f7e72510345118b5e984eec253c6fe644ef
ekateriinal <ekateriina.loginova@outlook.com>
2021-11-05 16:36:44 +0200

    Fixed first task

--- script.js
+++ script.js
@@ -1,59 +1,6 @@
-//Using loops (`for` or `while`), write a function, printing numbers from a to b, divisible by c.
-function numbersInLoops(a, b, c){
-    for (let i = a; i <= b; i++) {
-        if (i % c === 0) {   
-            return i
-        } 
-    }
+//Write a function, returning a sum of three numbers.
+
+function sum(a,b,c) {
+    return a+b+c
 }
-
-//calling:
-numbersInLoops(10, 20, 3); //prints 12
-
-
-//Write a function with will output a multiplication table with size of a given argument.
-function multiplicationTable(n){
-    
-    //TODO write your code
-    
-}
-//calling:
-//multiplicationTable(4);
-
-//Write a function, outputting a christmas tree of a given height, made of asterisk `*` character.
-function christmasTree(n){
-    
-    //TODO write your code
-    
-}
-//calling:
-//christmasTree(4);
-
-//Christmas Tree By Night
-function christmasTreeByNight(n){
-    
-    
-    //TODO write your code
-    
-}
-
-// Write a function which will accept 3 numbers and check, whether they form a pythagorean triple.
-// Modify the function so that it works, regardless the order of the arguments.
-function pythagoreanTriple(a, b, c){
-    
-    //TODO write your code
-    
-}
-//calling:
-//pythagoreanTriple(3,4,5);
-
-//Write a function which will count surface area of a given shape (triangle, rectangle, trapezoid), using `switch`.
-function surfaceArea(shape, a, b, c){
-    
-    
-    //TODO write your code
-    
-}
-//calling:
-//surfaceArea("triangle",2,5)
-
+sum(1,2,3)// 6
