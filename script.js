18eda949297b95efc3f94e70263239f95feac050
ekateriinal <ekateriina.loginova@outlook.com>
2021-11-05 15:46:19 +0200

    Added files from SDA

---
+++ script.js
@@ -0,0 +1,55 @@
+//Using loops (`for` or `while`), write a function, printing numbers from a to b, divisible by c.
+function numbersInLoops(a, b, c){
+    
+    //TODO write your code
+    
+}
+//calling:
+//numbersInLoops(10, 20, 3);
+
+//Write a function with will output a multiplication table with size of a given argument.
+function multiplicationTable(n){
+    
+    //TODO write your code
+    
+}
+//calling:
+//multiplicationTable(4);
+
+//Write a function, outputting a christmas tree of a given height, made of asterisk `*` character.
+function christmasTree(n){
+    
+    //TODO write your code
+    
+}
+//calling:
+//christmasTree(4);
+
+//Christmas Tree By Night
+function christmasTreeByNight(n){
+    
+    
+    //TODO write your code
+    
+}
+
+// Write a function which will accept 3 numbers and check, whether they form a pythagorean triple.
+// Modify the function so that it works, regardless the order of the arguments.
+function pythagoreanTriple(a, b, c){
+    
+    //TODO write your code
+    
+}
+//calling:
+//pythagoreanTriple(3,4,5);
+
+//Write a function which will count surface area of a given shape (triangle, rectangle, trapezoid), using `switch`.
+function surfaceArea(shape, a, b, c){
+    
+    
+    //TODO write your code
+    
+}
+//calling:
+//surfaceArea("triangle",2,5)
+
